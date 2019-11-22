44f82325e443e94e6b3cb1c6a3d3cb2e429c5705
Liz Jurkash <lizjurkash@Liz-Jurkashs-MacBook-Air.local>
2019-11-22 01:12:16 -0600

    Updated random_letter function

--- R/random_letter.R
+++ R/random_letter.R
@@ -1,6 +1,6 @@
 
 
-random_letter <- function(){
+random_letter <- function(vec = letter){
   letter <- c("A", "B", "C", "D", "E", "F","G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
   random <- sample(letter, 1)
   return(random)
