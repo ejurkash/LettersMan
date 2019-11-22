135dd90a18de22e316868b147bc72db6d40361bf
Liz Jurkash <lizjurkash@Liz-Jurkashs-MacBook-Air.local>
2019-11-22 00:55:53 -0600

    First Commit of Package

---
+++ R/random_letter.R
@@ -0,0 +1,7 @@
+
+
+random_letter <- function(){
+  letter <- c("A", "B", "C", "D", "E", "F","G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+  random <- sample(letter, 1)
+  return(random)
+}
